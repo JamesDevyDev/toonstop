fbbd0d1167d6838ccb67ed6875b1b325d43c0c51
James Talamo <jamestalamo4@gmail.com>
2025-05-22 19:29:57 +0800

    added drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import useManwhaStore from '@/zustand/useManwhaStore'
+import Drawer from './loading/drawer/Drawer'
 
 //Spacing px-[5%] md:px-[10%] lg:px-[20%]
 
@@ -30,21 +31,27 @@ const Header = () => {
 
     return (
         <div>
-            <div className='w-[100%] h-[90px] bg-[#d7af57] px-[5%] md:px-[10%] lg:px-[20%] relative flex items-center justify-start' >
+            <div className='w-[100%] h-[90px] bg-[#d7af57] px-[5%] md:px-[10%] lg:px-[20%] relative flex items-center justify-between' >
 
-                <div className='h-[100%] w-[150px] '>
-                    <Link href='/home/1'>
-                        <img src='/assets/logo.png' className='h-[100%] w-[100%] relative' />
-                    </Link>
+                <div className='flex items-center justify-center h-full relative'>
+                    <div className='h-[100%] w-[150px]'>
+                        <Link href='/home/1'>
+                            <img src='/assets/logo.png' className='h-[100%] w-[100%] relative' />
+                        </Link>
 
+                    </div>
+
+                    <div
+                        onClick={() => {
+                            matureSet()
+                        }}
+                        className={`relative cursor-pointer w-[80px] h-[30px]  border  rounded-lg text-[12px] flex items-center justify-center ${mature == 0 ? 'bg-green-500/10 border-green-500 text-green-500' : 'bg-red-500/10 border-red-500 text-red-500'} `}>
+                        {`NSFW ${mature == 0 ? 'OFF' : 'ON'}`} •
+                    </div>
                 </div>
 
-                <div
-                    onClick={() => {
-                        matureSet()
-                    }}
-                    className={`cursor-pointer w-[80px] h-[30px]  border  rounded-lg text-[12px] flex items-center justify-center ${mature == 0 ? 'bg-green-500/10 border-green-500 text-green-500' : 'bg-red-500/10 border-red-500 text-red-500'} `}>
-                    {`NSFW ${mature == 0 ? 'OFF' : 'ON'}`} •
+                <div>
+                    <Drawer />
                 </div>
 
             </div>
