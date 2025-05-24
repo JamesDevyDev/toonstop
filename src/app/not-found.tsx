28172027ea793be7c41e32bc130f1ad730c438ac
James Talamo <jamestalamo4@gmail.com>
2025-05-24 18:02:05 +0800

    not-found page fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
+
 export default function NotFound() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-black text-white">
-            <div className="text-center">
+        <div className="min-h-screen flex items-center justify-center bg-[#d7af57] px-4">
+            <div className="text-center flex items-center justify-center flex-col text-black">
+                <img src='/assets/logo.png' className='w-[300px] h-[300px]'/>
                 <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-                <p className="mt-4 text-gray-400">Sorry, the page you are looking for doesn't exist.</p>
+                <p className="mt-4 text-black">Sorry, the page you are looking for doesn't exist.</p>
+                <Link href='/home/1' className='p-[10px] bg-black text-white rounded-lg font-bold mt-[15px]'>Go Back to HOME.</Link>
             </div>
         </div>
     )
